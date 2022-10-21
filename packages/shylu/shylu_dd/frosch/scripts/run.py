0d6ef1f17fd0be780cd02b3352c814a9b22f5a4b
kaiserls <chess007@gmx.de>
2022-10-21 18:23:22 +0200

    [Scripts] Update run script

--- packages/shylu/shylu_dd/frosch/scripts/run.py
+++ packages/shylu/shylu_dd/frosch/scripts/run.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 import os
-import glob
 import shlex
 import shutil
-import sys
-import numpy as np
 import subprocess
 
-executable_path = "/faststorage/ianscluster08/kaiserls/Trilinos/build/packages/shylu/shylu_dd/frosch/test/Overlap/ShyLU_DDFROSch_overlap.exe"
-parameter_path = "/faststorage/ianscluster08/kaiserls/Trilinos/packages/shylu/shylu_dd/frosch/test/Overlap/ParameterLists/"
-hostfile_path = "/faststorage/ianscluster08/kaiserls/Trilinos/Hostfiles/"
+cluster = True
+
+if cluster:
+    executable_path = "/faststorage/ianscluster08/kaiserls/Trilinos/build/packages/shylu/shylu_dd/frosch/test/Overlap/ShyLU_DDFROSch_overlap.exe"
+    parameter_path = "/faststorage/ianscluster08/kaiserls/Trilinos/packages/shylu/shylu_dd/frosch/test/Overlap/ParameterLists/"
+    hostfile_path = "/faststorage/ianscluster08/kaiserls/Trilinos/Hostfiles/"
+else:
+    executable_path = "~/Git/Trilinos/build/packages/shylu/shylu_dd/frosch/test/Overlap/ShyLU_DDFROSch_overlap.exe"
+    parameter_path = "/home/lars/Git/Trilinos/packages/shylu/shylu_dd/frosch/test/Overlap/ParameterLists/"
+    hostfile_path = None
 
 def getParameterFile(mode):
     return parameter_path + f"ParameterList_OneLevelPreconditioner_{mode}.xml"
@@ -22,8 +26,13 @@ def getExecutableFile():
     return executable_path
 
 def getCommandString(N,M,O,mode):
-    mpi = f"nice -n 20 mpirun -n {N} -mca btl self,openib,vader,tcp -mca btl_tcp_if_exclude lo,docker0,wan1"
-    host = f"--hostfile {getHostFile(N)}"
+    if cluster:
+        mpi = f"nice -n 20 mpirun -n {N} -mca btl self,openib,vader,tcp -mca btl_tcp_if_exclude lo,docker0,wan1"
+        host = f"--hostfile {getHostFile(N)}"
+    else:
+        mpi = f"mpirun -n {N} --oversubscribe"
+        host = ""
+    
     exe = getExecutableFile()
     opts = f"--M={M} --DIM=2 --O={O} --DPN=1"
     plist = f"--PLIST={getParameterFile(mode)}"
@@ -46,14 +55,16 @@ def compute(output_path, Ms, Ns, Os, Modes, max_fails=10):
                     while res!=0 and fails<max_fails:
                         try:
                             with open(output_path+getOutFile(N,M,O,mode), "w") as outfile:
-                                cla = shlex.split(getCommandString(N,M,O,mode))
+                                cmd_string = getCommandString(N,M,O,mode)
+                                print(f"Executing {cmd_string}")
+                                cla = shlex.split(cmd_string)
                                 #cla = shlex.split("echo 1")
                                 res = subprocess.call(cla,cwd=output_path,stdout=outfile)
                                 if(res!=0): #failed
                                     fails=fails+1
                                     raise Exception()   
                         except:
-                            print(f"failed for {output_path+getOutFile(N,M,O,mode)}")
+                            print(f"Execution failed for {output_path+getOutFile(N,M,O,mode)}")
                             os.remove(output_path+getOutFile(N,M,O,mode))
                         yield
     print("---------------FINISHED---------------")
@@ -64,10 +75,10 @@ def main():
     output_path = os.getcwd()+'/'+'output/'
     cleanup(output_path)
 
-    Ms = [200,400,800]
-    Ns = [4,16,64,144]
-    Os = [8,12,20]
-    Modes = ["cg", "asho", "asho_onOverlapping", "rasho"]
+    Ms = [800]
+    Ns = [144]
+    Os = [12]
+    Modes = ["as", "asho", "asho_onOverlapping", "rasho", "ras"]
     computer = compute(output_path, Ms, Ns, Os, Modes)
 
     n = len(Ns)*len(Ms)*len(Os)*len(Modes)
